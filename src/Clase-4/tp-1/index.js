63fd74ddc6a5e56c42e964114b02e1309d41cda1
AzulPetrocelli <azulpetrocelli81@gmail.com>
2026-01-19 21:14:51 -0300

    Probando con una instancia de clase

--- src/Clase-4/tp-1/index.js
+++ src/Clase-4/tp-1/index.js
@@ -9,6 +9,49 @@ let products = [];
 // MANEJO DE PRODUCTOS
 //==========================================================
 
+//Clase ProductManager
+class ProductManager {
+	constructor() {
+		this.products = [];
+	}
+
+	addProduct(product) {
+		const newProduct = {
+			id: this.products.length + 1,
+			tittle: product.tittle,
+			description: product.description,
+			price: product.price,
+			thumbnail: product.thumbnail,
+			code: product.code,
+			stock: product.stock,
+		};
+
+		if (Object.values(newProduct).includes(undefined)) {
+			alert('Todos los campos son obligatorios');
+		} else if (this.getProducts().find((p) => p.code === newProduct.code)) {
+			alert('El codigo ingresado corresponde a otro producto');
+		} else {
+			this.products.push(newProduct);
+		}
+	}
+
+	getProducts() {
+		return this.products;
+	}
+
+	getProductById(id) {
+		const productFound = this.products.find((product) => product.id === id);
+
+		if (!productFound) {
+			console.error('Not Found');
+			return;
+		}
+
+		return this.products.find((product) => product.id === id);
+	}
+}
+
+
 //Retorna todos los productos
 server.get('/api/products/', (req, res) => {
     res.json({products});
